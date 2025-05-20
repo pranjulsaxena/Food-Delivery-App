10096ea642c3501899719de138180ed2f1fc3ed5
pranjulsaxena <saxena.pranjul2005@gmail.com>
2025-05-20 22:03:15 +0530

    Zod validation & Error handling for login page

--- Client/src/auth/login.tsx
+++ Client/src/auth/login.tsx
@@ -1,50 +1,103 @@
-import React, { useState, type ChangeEvent, type FormEvent } from 'react'
-import { Input } from "@/components/ui/input"
-import { Loader2, LockKeyhole, Mail } from 'lucide-react'
-import { Button } from '@/components/ui/button'
-import { Separator } from "@/components/ui/separator"
-import { Link } from 'react-router-dom'
-
+import React, { useState, type ChangeEvent, type FormEvent } from "react";
+import { Input } from "@/components/ui/input";
+import { Loader2, LockKeyhole, Mail } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Link } from "react-router-dom";
+import { userLoginSchema, type LoginInputState } from "@/schema/userSchema";
 
 function Login() {
-    interface InputTypes {
-        email: string,
-        password: string
-    }
-  
-    const [Loading, setLoading] = useState(false);
-    const [input, setinput] = useState<InputTypes>({ email: "", password: "" })
+  // interface LoginInputState {
+  //     email: string;
+  //     password: string;
+  // }
 
-      const changeEventHandler = (e:ChangeEvent<HTMLInputElement>)=>{
-        const {name,value} = e.target;
-        setinput({...input, [name]:value})
+  const [Loading, setLoading] = useState(false);
+  const [input, setinput] = useState<LoginInputState>({
+    email: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState<Partial<LoginInputState>>({});
+
+  const changeEventHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setinput({ ...input, [name]: value });
+  };
+  const formSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log(input);
+    const result = userLoginSchema.safeParse(input);
+    if (!result.success) {
+      const flattened = result.error.flatten();
+      setErrors(flattened.fieldErrors as Partial<LoginInputState>);
+      return;
     }
-        const formSubmit = (e:FormEvent)=>{
-            e.preventDefault();
-            console.log(input);
-        }
-    return (
-        <div className='min-h-screen flex  justify-center items-center'>
-            <form className='box-content max-w-md md:w-full rounded-lg border-black md:border md:p-8 text-center' onSubmit={formSubmit}>
-                <div className='mb-4'>
-                    <h1 className='font-bold text-2xl text-center'>InstaFoood</h1>
-                </div>
-                <div className='relative mb-4'>
-                    <Input type="email" placeholder='Email' value={input.email} className='pl-10 focus-visible:ring-1' name="email" onChange={changeEventHandler}></Input>
-                    <Mail className='absolute bottom-1 left-2 text-gray-500'></Mail>
-                </div>
-                <div className='relative mb-4'>
-                    <Input onChange={changeEventHandler} type="password" name='password' placeholder='Password'value={input.password} className='pl-10 focus-visible:ring-1'></Input>
-                    <LockKeyhole className='absolute bottom-1 left-2 text-gray-500'></LockKeyhole>
-                </div>
-                <div className='mb-10'>
-                    {Loading ? <Button disabled={true} className='w-full bg-[#D19254] hover:bg-[#d18c47] disabled:cursor-not-allowed'><Loader2 className='animate-spin mr-2 w-4 h-4'></Loader2>Loading ...</Button> : <Button type='submit' className='w-full bg-[#D19254] hover:bg-[#d18c47]'>Login</Button>}
-                </div>
-                <Separator />
-                <p className='mt-2'>Don't have an account? <Link to="/signup" className='text-blue-500'>SignUp</Link></p>
-            </form>
+  };
+  return (
+    <div className="min-h-screen flex  justify-center items-center">
+      <form
+        className="box-content max-w-md md:w-full rounded-lg border-black md:border md:p-8 text-center"
+        onSubmit={formSubmit}
+      >
+        <div className="mb-4">
+          <h1 className="font-bold text-2xl text-center">InstaFoood</h1>
         </div>
-    )
+        <div className="mb-4">
+          <div className="relative ">
+            <Input
+              type="email"
+              placeholder="Email"
+              value={input.email}
+              className="pl-10 focus-visible:ring-1"
+              name="email"
+              onChange={changeEventHandler}
+            ></Input>
+            <Mail className="absolute bottom-1 left-2 text-gray-500"></Mail>
+          </div>
+          {errors && <span className="text-sm text-red-500">{errors.email}</span>}
+        </div>
+        <div className="mb-4">
+          <div className="relative">
+            <Input
+              onChange={changeEventHandler}
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={input.password}
+              className="pl-10 focus-visible:ring-1"
+            ></Input>
+          <LockKeyhole className="absolute bottom-1 left-2 text-gray-500"></LockKeyhole>
+          </div>
+          {errors && <span className="text-sm text-red-500">{errors.password}</span>}
+        </div>
+        <div className="mb-10">
+          {Loading ? (
+            <Button
+              disabled={true}
+              className="w-full bg-[#D19254] hover:bg-[#d18c47] disabled:cursor-not-allowed"
+            >
+              <Loader2 className="animate-spin mr-2 w-4 h-4"></Loader2>Loading
+              ...
+            </Button>
+          ) : (
+            <Button
+              type="submit"
+              className="w-full bg-[#D19254] hover:bg-[#d18c47]"
+            >
+              Login
+            </Button>
+          )}
+        </div>
+        <Separator />
+        <p className="mt-2">
+          Don't have an account?{" "}
+          <Link to="/signup" className="text-blue-500">
+            SignUp
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
