fded9c822fa7beabda4d0d47e0f714f806e9e8ae
pranjulsaxena <saxena.pranjul2005@gmail.com>
2025-06-21 23:26:33 +0530

    Restaurant search page UI

---
+++ Client/src/components/SearchPage.tsx
@@ -0,0 +1,136 @@
+import React, { useState, type ChangeEvent } from "react";
+import { Link, useParams, useSearchParams } from "react-router-dom";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import Filter from "./Filter";
+import { Badge } from "./ui/badge";
+import { Globe, MapPin, X } from "lucide-react";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import pizzaImage from "@/assets/pizza.png";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import SearchPageSkeleton from "./SearchPageSkeleton";
+
+
+function SearchPage() {
+  const Params = useParams();
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
+  return (
+    <div className="max-w-7xl mx-auto px-4 md:px-8 flex flex-col md:flex-row gap-2 my-10">
+      <Filter />
+      <div className="flex-1 flex flex-col">
+        <div className="w-full flex gap-2 px-1">
+          <Input
+            placeholder="Search by restaurant & cuisines"
+            onChange={(e) => {
+              setSearchQuery(e.target.value);
+            }}
+            className="w-full focus-visible:ring-0 focus-visible:border-2 focus-visible:border-black"
+          ></Input>
+          <Button className="bg-[#D19254] text-white">Search</Button>
+        </div>
+
+        <div className="">
+          <div className="flex flex-col gap-3 md:flex-row md:items-center md:gap-2 my-3 ">
+            <div className="">
+              <h2 className="font-bold">(2) Search results found</h2>
+              <div>
+                {["Biryani", "Momos", "Chole Bhature"].map(
+                  (selectedFilter, idx) => (
+                    <div key={idx} className="inline-flex mr-2 relative">
+                      <Badge
+                        className="bg-white text-[#D19254] w-full flex gap-2 items-center rounded-md hover:cursor-pointer"
+                        variant="outline"
+                      >
+                        <span className="md:text-md">{selectedFilter}</span>
+                        <div className="">
+                          <X className="size-4" />
+                        </div>
+                      </Badge>
+                    </div>
+                  )
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Searched items displayed here  */}
+        <div className="grid md:grid-cols-3 gap-4 mx-auto md:w-full">
+          {[1, 2, 3, 4,5].map((_, index) => (
+            <Card
+              key={index}
+              className="w-[300px] overflow-hidden rounded-md shadow-2xl pt-6 pb-0"
+            >
+              <div className="relative">
+                <AspectRatio ratio={16 / 9}>
+                  <img
+                    src={pizzaImage}
+                    alt="Delicious pizza"
+                    className="size-full  object-cover rounded-md"
+                  />
+                </AspectRatio>
+                <div className="absolute text-gray-700 top-1 left-1 bg-white/65 rounded-md px-2 py-1">
+                  Featured
+                </div>
+              </div>
+              <CardContent className="flex flex-col gap-2">
+                <h1 className="text-xl font-bold">Pizza Hunt</h1>
+
+                <div className="flex items-center  text-gray-600 text-sm">
+                  <MapPin className=" mr-1" />
+                  City: <span className="font-bold mx-1"> Delhi</span>
+                </div>
+                <div className="flex text-gray-600 text-sm">
+                  <Globe className="mr-1" /> Country:
+                  <span className="font-bold mx-1">India</span>
+                </div>
+
+                <div>
+                  {["Pizza", "Momos", "Burger"].map(
+                    (cuisine: string, idx: number) => (
+                      <div key={idx} className="inline-flex mr-2 relative">
+                        <Badge
+                          className=" w-full flex gap-2 items-center rounded-md hover:cursor-pointer"
+                          variant="default"
+                        >
+                          <span className="md:text-md">{cuisine}</span>
+                        </Badge>
+                      </div>
+                    )
+                  )}
+                </div>
+              </CardContent>
+              <CardFooter className="p-4 border-t dark:border-t-gray-700 border-t-gray-100 text-white flex justify-end">
+                <Button className="rounded-full bg-[#D19254] hover:bg-[#d18c47] transition-colors duration-300 font-semibold shadow-md px-4 pt-2">
+                  View More
+                </Button>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const NoResultFound = ({ searchText }: { searchText: string }) => {
+  return (
+    <div className="text-center">
+      <h1 className="text-2xl font-semibold text-gray-700 dark:text-gray-200">
+        No results found
+      </h1>
+      <p className="mt-2 text-gray-500 dark:text-gray-400">
+        We couldn't find any results for "{searchText}". <br /> Try searching
+        with a different term.
+      </p>
+      <Link to="/">
+        <Button className="mt-4 bg-orange hover:bg-orangeHover">
+          Go Back to Home
+        </Button>
+      </Link>
+    </div>
+  );
+};
+
+export default SearchPage;
