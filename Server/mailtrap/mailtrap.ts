581ee9e0ffbc100de93f821bfda914b7d59d880f
pranjulsaxena <saxena.pranjul2005@gmail.com>
2025-06-28 23:47:54 +0530

    add email service for verification, welcome, password reset, and reset success

---
+++ Server/mailtrap/mailtrap.ts
@@ -0,0 +1,12 @@
+const { MailtrapClient } = require("mailtrap");
+
+
+
+export const client = new MailtrapClient({
+  token: process.env.MAILTRAP_API_TOKEN
+});
+
+export const sender = {
+  email: "hello@demomailtrap.co",
+  name: "InstaFood",
+};
