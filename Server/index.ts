6f6e8a928cfb89737cba4cb45f2e52bb1a748b38
pranjulsaxena <saxena.pranjul2005@gmail.com>
2025-06-29 23:54:13 +0530

    user auth routes with profile update and password reset

--- Server/index.ts
+++ Server/index.ts
@@ -1,8 +1,25 @@
 import express from "express"
 import 'dotenv/config'
 import connectDB from "./db/connectDB";
+import userRoutes from "./routes/user.routes"
+import bodyParser from "body-parser"
+import cookieParser from "cookie-parser";
+import cors from "cors"
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT||5000;
+
+
+// app.use(bodyParser.json({limit:'10mb'}));
+app.use(express.urlencoded({extended:true,limit:'10mb'}));
+app.use(express.json({limit:"10mb"}));
+app.use(cookieParser());
+const corsOptions = {
+    origin:"https://localhost:5173",
+    credentials:true
+}
+app.use(cors(corsOptions));
+
+app.use("/api/v1/user",userRoutes);
 
 app.listen(PORT, ()=>{
     connectDB();
---
+++ Server/routes/user.routes.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import {
+  signup,
+  login,
+  verifyEmail,
+  logout,
+  forgotPassword,
+  resetPassword,
+  checkAuth,
+  updateProfile,
+} from "../controller/user.controller";
+import { isAuthenticated } from "../middleware/isAuthenticated";
+const router = express.Router();
+
+router.route("/check-ath").post(isAuthenticated,checkAuth);
+router.route("/signup").post(signup);
+router.route("/login").post(login);
+router.route("/logout").post(logout);
+router.route("/verify-email").post(verifyEmail);
+router.route("/forgot-password").post(forgotPassword);
+router.route("/reset-password/:token").post(resetPassword);
+router.route("/profile/update").put(isAuthenticated,updateProfile);
+
+export default router
+
+
